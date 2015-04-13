b07779ee78387c94c34015b89e34e24cb64a7923
Jamie Patel <jamie@notespublication.com>
2015-04-13 14:55:38 +0100

    Made handshake the responsibility of the server

--- websocket_connection.rb
+++ websocket_connection.rb
@@ -1,13 +1,11 @@
 class WebsocketConnection
 
-  WS_MAGIC_STRING = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
   OPCODE_TEXT = 0x01
 
-  attr_reader :socket, :path, :handshake_made
+  attr_reader :socket, :path
 
   def initialize(socket, path)
-    @socket, @path, @handshake_made = socket, path, false
-    begin_handshake
+    @socket, @path = socket, path
   end
 
   def listen(&block)
@@ -54,44 +52,6 @@ class WebsocketConnection
     bytes += message.bytes
     data = bytes.pack("C*")
     socket << data
-  end
-
-  private
-
-  def begin_handshake 
-    request_line = socket.gets
-    if request_line =~ /GET #{path}/
-      header = get_header
-      return send_400 if !(header =~ /Sec-WebSocket-Key: (.*)\r\n/)
-      ws_accept = create_websocket_accept($1)
-      send_handshake_response(ws_accept)
-      @handshake_made = true
-    end
-  end
-
-  def get_header(header = "")
-    (line = socket.gets) == "\r\n" ? header : get_header(header + line)
-  end
-
-  def send_400
-    socket << "HTTP/1.1 400 Bad Request\r\n" +
-              "Content-Type: text/plain\r\n" +
-              "Connection: close\r\n" +
-              "\r\n" +
-              "Incorrect request"
-    socket.close   
-  end
-
-  def send_handshake_response(ws_accept)
-    socket << "HTTP/1.1 101 Switching Protocols\r\n" +
-              "Upgrade: websocket\r\n" +
-              "Connection: Upgrade\r\n" +
-              "Sec-WebSocket-Accept: #{ws_accept}\r\n" 
-  end
-
-  def create_websocket_accept(key)
-    digest = Digest::SHA1.digest(key + WS_MAGIC_STRING)
-    Base64.encode64(digest)
   end
 
 end
--- websocket_server.rb
+++ websocket_server.rb
@@ -5,6 +5,8 @@ require_relative 'websocket_connection'
 
 class WebsocketServer
 
+  WS_MAGIC_STRING = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+
   def initialize(options={path: '/', port: 4567, host: 'localhost'})
     @path, port, host = options[:path], options[:port], options[:host]
     @tcp_server = TCPServer.new(host, port)
@@ -13,10 +15,49 @@ class WebsocketServer
   def connect(&block)
     loop do
       Thread.start(@tcp_server.accept) do |socket|
-          connection = WebsocketConnection.new(socket, @path)
-          yield(connection) if connection.handshake_made
+        send_handshake(socket) && yield(WebsocketConnection.new(socket, @path))
       end
     end
+  end
+
+  private
+
+  def send_handshake(socket)
+    request_line = socket.gets
+    if request_line =~ /GET #{@path}/
+      header = get_header(socket)
+      return send_400(socket) if !(header =~ /Sec-WebSocket-Key: (.*)\r\n/)
+      ws_accept = create_websocket_accept($1)
+      send_handshake_response(socket, ws_accept)
+      return true
+    end
+    false
+  end
+
+  def get_header(socket, header = "")
+    (line = socket.gets) == "\r\n" ? header : get_header(socket, header + line)
+  end
+
+  def send_400(socket)
+    socket << "HTTP/1.1 400 Bad Request\r\n" +
+              "Content-Type: text/plain\r\n" +
+              "Connection: close\r\n" +
+              "\r\n" +
+              "Incorrect request"
+    socket.close
+    false  
+  end
+
+  def send_handshake_response(socket, ws_accept)
+    socket << "HTTP/1.1 101 Switching Protocols\r\n" +
+              "Upgrade: websocket\r\n" +
+              "Connection: Upgrade\r\n" +
+              "Sec-WebSocket-Accept: #{ws_accept}\r\n" 
+  end
+
+  def create_websocket_accept(key)
+    digest = Digest::SHA1.digest(key + WS_MAGIC_STRING)
+    Base64.encode64(digest)
   end
 
 end
